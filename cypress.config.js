15f95a2b6859eb2167a0efeb4ca33ab4241a1ce8
Hyeonjong <nianelo4@gmail.com>
2023-02-19 11:19:55 +0900

    Add some configs for component testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,21 @@
 const { defineConfig } = require("cypress");
-const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
+const {
+  addMatchImageSnapshotPlugin,
+} = require("cypress-image-snapshot/plugin");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5000',
+    baseUrl: "http://localhost:5000",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       addMatchImageSnapshotPlugin(on, config);
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
     },
   },
 });
